7bc78720b29fecdc77e3fa8f97deb3a64a01ebc0
Johannes Eichhorn <johannes@eichhorn-networks.de>
2025-07-28 13:35:42 +0200

    fresh compiled

---
+++ src/code/build/CMakeFiles/compile_win64.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/compile_win64"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/compile_win64.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
