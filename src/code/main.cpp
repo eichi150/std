72314b26b677125c1699832c823c8fc9accd9066
Johannes Eichhorn <johannes@eichhorn-networks.de>
2025-07-31 18:40:16 +0200

    environment deaktiviert. muss besser strukturiert werden

--- src/code/main.cpp
+++ src/code/main.cpp
@@ -40,9 +40,7 @@ int main(int argc, char* argv[]){
 		try{
 			
 			//Argumente entgegen nehmen und Parsen
-			
 			std::vector<std::string> str_argv = ctrl->parse_argv(argc, argv);
-
 			if(ctrl->is_argument_valid(str_argv)){
 				
 				std::shared_ptr<JSON_Handler> jsonH = std::make_shared<JSON_Handler>();
@@ -68,67 +66,23 @@ int main(int argc, char* argv[]){
 				std::shared_ptr<Arg_Manager> arg_man = std::make_shared<Arg_Manager>(jsonH, ctrl);
 				
 				arg_man->proceed_inputs(argc, str_argv);
-				
-				std::unique_ptr<CLI_UI> cli = std::make_unique<CLI_UI>(arg_man, ctrl->get_log());
-
-				//Environment
-				if(arg_man->run_environment()){
-					std::cout << "std - Environment started... |-- Close with 'exit'" << std::endl;
-					do{
-						bool arg_valid = false;
-						std::vector<std::string> new_argv{"std"};
-						
-						do{
-							//New Inputs parsen
-							std::string input;
-							std::cout << "@std > ";
-							std::getline(std::cin, input);
-												
-							if(input == "exit"){
-								return 0;
-							}
-							
-							new_argv = ctrl->split_input(input);
-							new_argv.insert(new_argv.begin(), "std");
-							new_argv = ctrl->parse_argv(new_argv.size(), new_argv);
-							arg_valid = ctrl->is_argument_valid(new_argv);
-							
-							for(const auto& arg : new_argv){
-								std::cout << arg << '\n';
-							}
-							
-						}while(!arg_valid);
-						
-						str_argv = new_argv;
-						argc = static_cast<int>(str_argv.size());
-
-						try{
-				
-							arg_man->proceed_inputs(argc, str_argv);
-							
-						}catch(const std::runtime_error& rt){
-							std::cout << "**" << rt.what() << std::endl;
-						}
-					}while(arg_man->run_environment());
-				}
-
 				//CLI UserInterface & ConsoleOutput				
+				std::unique_ptr<CLI_UI> cli = std::make_unique<CLI_UI>(arg_man, ctrl->get_log());
 				cli->update();
-
 				
-			}else{
-			
-				throw std::runtime_error{ctrl->get_str_error().at(error::unknown)};
+				return 0;
 			}
+			
+			throw std::runtime_error{ctrl->get_str_error().at(error::unknown)};
+			
 		//Error Output	
 		}catch(const std::runtime_error& re){
 			std::cerr << "**" << re.what() << "\n" << ctrl->get_log() << std::endl;
 		}
 		
-	}else{
-	
-		std::cout << "Simple Time Documentation - github.com/eichi150/std" << std::endl;
 	}
+	
+	std::cout << "Simple Time Documentation - github.com/eichi150/std" << std::endl;
 	
 	return 0;
 }
